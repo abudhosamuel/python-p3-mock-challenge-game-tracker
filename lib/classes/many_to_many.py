b104ec8d385d5eff05dfd5385509b64256acc4e2
abudhosamuel <abudhosamuel@gmail.com>
2024-09-12 19:41:27 +0300

    mock challenge game tracker

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,99 @@
 class Game:
     def __init__(self, title):
-        self.title = title
+        # Validation to ensure title is a string and longer than 0 characters
+        if not isinstance(title, str) or len(title) == 0:
+            raise Exception("Title must be a non-empty string.")
+        self._title = title  # Use _title to make it immutable
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        # Raise an exception if someone tries to change the title
+        raise AttributeError("Cannot modify the title once set.")
+
 
     def results(self):
-        pass
+        # Return a list of all results for this game
+        return [result for result in Result.all_results if result.game == self]
 
     def players(self):
-        pass
+        # Return a unique list of all players that played this game
+        return list(set([result.player for result in self.results()]))
 
     def average_score(self, player):
-        pass
+        # Return the average score of the player for this game
+        player_results = [result.score for result in self.results() if result.player == player]
+        if player_results:
+            return sum(player_results) / len(player_results)
+        return 0
+
 
 class Player:
     def __init__(self, username):
+        # Validate that the username is a string and between 2 and 16 characters
+        if not isinstance(username, str) or not (2 <= len(username) <= 16):
+            raise Exception("Username must be a string between 2 and 16 characters.")
         self.username = username
 
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, value):
+        if not isinstance(value, str) or not (2 <= len(value) <= 16):
+            raise Exception("Username must be a string between 2 and 16 characters.")
+        self._username = value
+
     def results(self):
-        pass
+        # Return a list of all results for this player
+        return [result for result in Result.all_results if result.player == self]
 
     def games_played(self):
-        pass
+        # Return a unique list of all games played by the player
+        return list(set([result.game for result in self.results()]))
 
     def played_game(self, game):
-        pass
+        # Check if the player played a specific game
+        return any(result.game == game for result in self.results())
 
     def num_times_played(self, game):
-        pass
+        # Return the number of times the player played a specific game
+        return sum(1 for result in self.results() if result.game == game)
+
+    @classmethod
+    def highest_scored(cls, game):
+        # Return the player with the highest average score in the game
+        players = game.players()
+        if not players:
+            return None
+        return max(players, key=lambda player: game.average_score(player))
 
 class Result:
+    all_results = []  # List to track all results globally
+
     def __init__(self, player, game, score):
-        self.player = player
-        self.game = game
-        self.score = score
+        # Validate that player is a Player instance, game is a Game instance, and score is a valid int
+        if not isinstance(player, Player) or not isinstance(game, Game):
+            raise Exception("Invalid player or game instance.")
+        if not isinstance(score, int) or not (1 <= score <= 5000):
+            raise Exception("Score must be an integer between 1 and 5000.")
+        self._player = player
+        self._game = game
+        self._score = score
+        Result.all_results.append(self)  # Add this result to the global list
+
+    @property
+    def player(self):
+        return self._player
+
+    @property
+    def game(self):
+        return self._game
+
+    @property
+    def score(self):
+        return self._score
--- lib/testing/game_test.py
+++ lib/testing/game_test.py
@@ -1,114 +1,104 @@
-import pytest
+class Game:
+    def __init__(self, title):
+        # Ensure title is a non-empty string
+        if not isinstance(title, str) or len(title) == 0:
+            raise Exception("Title must be a non-empty string.")
+        self._title = title  # Make title immutable by using a private variable
 
-from classes.many_to_many import Player
-from classes.many_to_many import Game
-from classes.many_to_many import Result
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        # Raise an exception if someone tries to change the title
+        raise AttributeError("Cannot modify the title once set.")
+
+    def results(self):
+        # Return a list of all results for this game
+        return [result for result in Result.all_results if result.game == self]
+
+    def players(self):
+        # Return a unique list of all players who played this game
+        return list(set([result.player for result in self.results()]))
+
+    def average_score(self, player):
+        # Calculate the average score for a player in this game
+        player_results = [result.score for result in self.results() if result.player == player]
+        if player_results:
+            return sum(player_results) / len(player_results)
+        return 0
 
 
-class TestGame:
-    """Game in many_to_many.py"""
+class Player:
+    def __init__(self, username):
+        # Ensure username is a valid string and between 2 and 16 characters
+        if not isinstance(username, str) or not (2 <= len(username) <= 16):
+            raise Exception("Username must be a string between 2 and 16 characters.")
+        self.username = username
 
-    def test_has_title(self):
-        """Game is initialized with a title"""
-        game_1 = Game("Skribbl.io")
-        game_2 = Game("Jenga")
+    @property
+    def username(self):
+        return self._username
 
-        assert game_1.title == "Skribbl.io"
-        assert game_2.title == "Jenga"
+    @username.setter
+    def username(self, value):
+        # Ensure username is between 2 and 16 characters
+        if not isinstance(value, str) or not (2 <= len(value) <= 16):
+            raise Exception("Username must be a string between 2 and 16 characters.")
+        self._username = value
 
-    def test_title_is_immutable_string(self):
-        """title is an immutable string"""
-        game = Game("Skribbl.io")
-        assert isinstance(game.title, str)
 
-        # comment out the next two lines if using Exceptions
-        game.title = 2
-        assert game.title == "Skribbl.io"
+    @property
+    def username(self):
+        return self._username
 
-        # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     game.title = "not Skribbl.io"
+    @username.setter
+    def username(self, value):
+        # Ensure username is between 2 and 16 characters
+        if not isinstance(value, str) or not (2 <= len(value) <= 16):
+            raise Exception("Username must be a string between 2 and 16 characters.")
+        self._username = value
 
-    def test_title_len(self):
-        """title is greater than 0 characters"""
-        game = Game("Skribbl.io")
+    def results(self):
+        # Return a list of all results for this player
+        return [result for result in Result.all_results if result.player == self]
 
-        assert hasattr(game, "title")
-        assert len(game.title) > 0
+    def games_played(self):
+        # Return a unique list of all games this player has played
+        return list(set([result.game for result in self.results()]))
 
-        # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Game("")
+    def played_game(self, game):
+        # Check if the player has played a specific game
+        return any(result.game == game for result in self.results())
 
-    def test_has_many_results(self):
-        """game has many results"""
-        game = Game("Skribbl.io")
-        game_2 = Game("Scattegories")
-        player = Player("Saaammmm")
-        result_1 = Result(player, game, 2000)
-        result_2 = Result(player, game, 3500)
-        result_3 = Result(player, game_2, 19)
+    def num_times_played(self, game):
+        # Return the number of times the player played a specific game
+        return sum(1 for result in self.results() if result.game == game)
 
-        assert len(game.results()) == 2
-        assert result_1 in game.results()
-        assert result_2 in game.results()
-        assert result_3 not in game.results()
 
-    def test_results_of_type_result(self):
-        """game results are of type Result"""
-        game = Game("Skribbl.io")
-        player = Player("Saaammmm")
-        Result(player, game, 2000)
-        Result(player, game, 3500)
+class Result:
+    all_results = []  # Global list to track all results
 
-        assert isinstance(game.results()[0], Result)
-        assert isinstance(game.results()[1], Result)
+    def __init__(self, player, game, score):
+        # Ensure player is an instance of Player, game is an instance of Game, and score is valid
+        if not isinstance(player, Player) or not isinstance(game, Game):
+            raise Exception("Invalid player or game instance.")
+        if not isinstance(score, int) or not (1 <= score <= 5000):
+            raise Exception("Score must be an integer between 1 and 5000.")
+        self._player = player
+        self._game = game
+        self._score = score
+        Result.all_results.append(self)  # Add this result to the global list
 
-    def test_has_many_players(self):
-        """game has many players"""
-        game = Game("Skribbl.io")
+    @property
+    def player(self):
+        return self._player
 
-        player = Player("Nick")
-        player_2 = Player("Ari")
-        player_3 = Player("Saaammmm")
-        Result(player, game, 5000)
-        Result(player_2, game, 4999)
+    @property
+    def game(self):
+        return self._game
 
-        assert player in game.players()
-        assert player_2 in game.players()
-        assert player_3 not in game.players()
-
-    def test_players_of_type_player(self):
-        """game players are of type Player"""
-        game = Game("Skribbl.io")
-        player = Player("Nick")
-        player_2 = Player("Ari")
-        Result(player, game, 5000)
-        Result(player_2, game, 4999)
-
-        assert isinstance(game.players()[0], Player)
-        assert isinstance(game.players()[1], Player)
-
-    def test_has_unique_players(self):
-        """game players are unique"""
-        game = Game("Skribbl.io")
-
-        player = Player("Nick")
-        player_2 = Player("Ari")
-        Result(player, game, 5000)
-        Result(player, game, 5002)
-        Result(player_2, game, 4999)
-
-        assert len(set(game.players())) == len(game.players())
-        assert len(game.players()) == 2
-
-    def test_average_score(self):
-        """game can calculate a player's average score"""
-        game = Game("Skribbl.io")
-        player = Player("Nick")
-        Result(player, game, 5000)
-        Result(player, game, 4999)
-        Result(player, game, 5000)
-        Result(player, game, 4999)
-
-        assert game.average_score(player) == 4999.5
+    @property
+    def score(self):
+        return self._score
